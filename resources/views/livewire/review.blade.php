16d323200728e8698a873ad2fb6e13b6a16cbf3d
Riju-88 <rijumistri4@gmail.com>
2024-05-31 21:31:01 +0530

    Added new layout for Product Details page. Attributes not added yet.

--- resources/views/livewire/review.blade.php
+++ resources/views/livewire/review.blade.php
@@ -1,72 +1,65 @@
-<div>
-    <div class="max-w-5xl mx-auto sm:px-6 lg:px-8">
- @if (Auth::check())
-        <form wire:submit="create" x-ref="form">
-
-            <div class="fi-form-group my-6">
-                {{-- if auth check --}}
-               
-                {{ $this->form }}
-             
-            </div>
-
-            <button class="btn btn-primary">
-                {{ $editing ? 'Update' : 'Submit' }}
-            </button>
-        </form>
-   @endif
+<div class="container px-5 py-24 mx-auto">
+    <div class="lg:w-4/5 mx-auto sm:px-6 lg:px-8">
+        {{-- Authentication Check --}}
+        @if (Auth::check())
+            <form wire:submit="create" x-ref="form">
+                <div class="fi-form-group my-6">
+                    {{-- Form Content --}}
+                    {{ $this->form }}
+                </div>
+                <button class="btn btn-primary">
+                    {{ $editing ? 'Update' : 'Submit' }}
+                </button>
+            </form>
+        @endif
         <x-filament-actions::modals />
         <livewire:notifications />
-
     </div>
-
 
     <div class="mt-8 flex flex-col mx-4">
-        <div class="">
+        
             <h2 class="text-xl font-bold">Reviews</h2>
-
+    
             @foreach ($reviews as $review)
-                {{-- if the product id matches the current product id --}}
+            <div class="mt-4" wire:key="review-{{ $review->id }}">
+                {{-- Check if the product id matches the current product id --}}
                 @if ($productId == $review->product_id)
-                    <div class="mt-4">
-                        <h3 class="text-lg font-bold">{{ $review->user->name }}</h3>
-                        <div class="text-sm text-gray-500">{{ $review->created_at->format('M d, Y') }}</div>
-                        <div class="mt-2">
-                            @for ($i = 0; $i < $review->rating; $i++)
-                                ⭐️
-                            @endfor
-                        </div>
-                        <p class="mt-2">{{ $review->title }}</p>
-                        <p class="mt-2">{!! $review->review !!}</p>
+                
+                    <h3 class="text-lg font-bold">{{ $review->user->name }}</h3>
+                    <div class="text-sm text-gray-500">{{ $review->created_at->format('M d, Y') }}</div>
+                    <div class="flex items-center mt-2">
+                        @for ($i = 0; $i < $review->rating; $i++)
+                            <span class="text-yellow-500">⭐️</span>
+                        @endfor
                     </div>
-                    <div>
+                    <p class="mt-2">{{ $review->title }}</p>
+                    <p class="mt-2">{!! $review->review !!}</p>
+                    <div class="grid grid-cols-3 gap-4 mt-2">
                         @foreach ($review->image as $filePath)
-                            <img src="{{ asset('storage/' . $filePath) }}" class="mt-2 max-w-sm" alt="">
+                            <img src="{{ asset('storage/' . $filePath) }}" class="max-w-xs" alt="" wire:key="image-file-{{ $review->id }}">
+                        @endforeach
                     </div>
-                @endforeach
-                <div class="mt-4">
-                    @if (Auth::check())
-                    <!-- edit button for review -->
-                    <button wire:click="edit({{ $review->id }})"
-                        @click="$refs.form.scrollIntoView({ behavior: 'smooth' })" class="btn btn-primary">Edit</button>
-
-                    <!-- <button wire:click="delete({{ $review->id }})" class="btn btn-error">Delete</button> -->
-
-
-                    {{-- Delete Review --}}
-                    <!-- Open the modal using ID.showModal() method -->
-                    {{ ($this->delete)(['review_id' => $review->id]) }}
-
-
-                    @endif
-
-
-                    {{--  --}}
-
-
-                </div>
-            @endif
+                    <div class="mt-4">
+                        @if (Auth::check())
+                            <!-- Edit button for review -->
+                            <button wire:click="edit({{ $review->id }})"
+                                @click="$refs.form.scrollIntoView({ behavior: 'smooth' })"
+                                class="btn btn-primary">Edit
+                            </button>
+                
+                            <!-- Delete Review -->
+                            {{ $this->delete(['review_id' => $review->id]) }}
+                        @endif
+                    </div>
+                    {{-- End of auth --}}
+                
+                
+                
+                @endif
+            </div>
             @endforeach
-        </div>
+        
     </div>
+    
+
 </div>
